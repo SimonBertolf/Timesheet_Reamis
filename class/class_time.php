8a01ff7d9562be58b4ab545d038b71736ac3c73e
Simon Bertolf <simon.bertolf@reamis.ch>
2019-12-05 12:55:35 +0100

    class time erstellt

---
+++ class/class_time.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class class_time
+{
+    private $start;
+    private $stop;
+    private $diff;
+    private $work_time;
+
+    public function work_time($start_time,$stop_time){
+
+        $this->start = new DateTime($start_time);
+        $this->stop = new DateTime($stop_time);
+
+        $this->diff = $this->start->diff($this->stop);
+
+        $this->work_time = $this->diff->format("%h").'.'.$this->diff->format("%I")/6*10;
+        echo $this->work_time;
+        return $this->work_time;
+    }
+
+}
