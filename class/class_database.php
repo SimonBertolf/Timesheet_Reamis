3c9686e3b8c2b119a5650326b8f02c71f5fe0de5
Simon Bertolf <simon.bertolf@reamis.ch>
2019-12-05 10:20:07 +0100

    Logos und loginpage erstellt

--- class/class_database.php
+++ class/class_database.php
@@ -1,7 +1,38 @@
 <?php
 
-
-class class_database
+class database
 {
+    // User Daten
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pw = 'root';
+    private $db = 'timesheet_reamis';
+    public $mysql;
 
+    // Funktionen
+    public function __construct()
+    {
+    }
+
+    public function close_connection()
+    {
+        /// -------- Verbindung trennen --------
+        $this->mysql->close();
+    }
+
+    public function open_connection()
+    {
+    /// -------- Verbindungsaufbau --------
+    $this->mysql = new mysqli($this->host, $this->user, $this->pw, $this->db);
+
+    /// -------- Fehleranalyse --------
+        if ($this->mysql === false) {
+            die('Verbindungsfehler' . $this->mysql->connect_error . "<br />");
+        }
+        else if ($this->mysql->connect_error) {
+            die('Fehler' . $this->mysql->connect_error . "<br />");
+        }
+    }
 }
+
+
--- page/page_login.php
+++ page/page_login.php
@@ -1,1 +1,27 @@
 <?php
+require_once '../system/system_login.php';
+?>
+<html>
+    <head>
+        <link href="../CSS/timesheet_font.css" rel="stylesheet" type="text/css">
+        <link href="../CSS/timesheet_style.css" rel="stylesheet" type="text/css">
+        <title>TimeSheet</title>
+    </head>
+    <body id="around">
+        <div>
+            <div>
+                <img id="image_login" src="../image/timesheet_login_logo.svg">
+            </div>
+            <div>
+                <form method="post" class="div_flex_colum">
+                    <input class="input_01" id="login" type="email" name="email" placeholder="E-mail" required>
+                    <input class="input_01" id="login" type="password" name="pasword" placeholder="Passwort" required>
+                    <button class="button_01" id="login" name="login">Login</button>
+                    <p class="font_error"><?php if (isset($errorMessage)){ echo$errorMessage;}?></p>
+                </form>
+            </div>
+        </div>
+    </body>
+    <footer>
+    </footer>
+</html>
