d2b19edef50efbba39b76a77197e8cbd9b54c16c
Simon Bertolf <simon.bertolf@reamis.ch>
2019-12-17 10:40:35 +0100

    Archivieren hinzugefügt

--- page/page_project.php
+++ page/page_project.php
@@ -30,18 +30,18 @@ if ($_SESSION['user_typ'] == 'controller' || $_SESSION['user_typ'] == 'admin'){
         <div class="button_03_slider" id='3' >Add new project</div>
         <div class="div_slider" id="4">
             <form method="post" class="div_flex_colum">
-                <input class="input_01" id="navigation" type="text" name="projectname" placeholder="Projectname">
-                <input class="input_01" id="navigation"  type="text" name="desription" placeholder="description">
-                <input class="input_01" id="navigation"  type="number" name="budget" placeholder="Budget">
+                <input class="input_01" id="navigation" type="text" name="projectname" placeholder="Projectname" required>
+                <input class="input_01" id="navigation"  type="text" name="desription" placeholder="description" required>
+                <input class="input_01" id="navigation"  type="number" name="budget" placeholder="Budget" required>
                 <button class="button_01" id="navigation" name="add" >Add</button>
             </form>
         </div>
         <div class="div_flex_row">
             <div class="div_left">
-                <form method="post" class="div_flex_colum">
+                <form method="get" class="div_flex_colum">
                 <?php
                 while ($res = $query->fetch_assoc()){
-                    echo ('<button class="button_01" id="button_project" name="project" value='.$res['projectname'].'>'.$res['projectname'].'</button>');
+                    echo ('<button class="button_01" id="button_project" name="Test" value='.$res['projectname'].'>'.$res['projectname'].'</button>');
                 }
                 ?>
                 </form>
@@ -49,7 +49,7 @@ if ($_SESSION['user_typ'] == 'controller' || $_SESSION['user_typ'] == 'admin'){
             <div class="div_right" >
                 <div>
                  <?php
-                 if (isset($_POST['project'])) {
+                 if (isset($_GET['Test'])) {
                      echo '<p class="font_01">'.$project['projectname'].'<p>';
                      echo '<p class="font_02">'.$project['description'].'<p>';
                      echo '<p class="font_02">Projectnumber: '.$project['nr'].'<p>';
@@ -57,7 +57,7 @@ if ($_SESSION['user_typ'] == 'controller' || $_SESSION['user_typ'] == 'admin'){
                  ?>
                  </div>
                 <div id="columnchart_material"></div>
-                     <form method="post" id="delete">
+                     <form method="post" id="delete" action="page_project.php?Test=<?php echo $_SESSION['name'];?>">
                          <button class="button_01" id="button_project" name="archive">Archive <?php echo $project['archive'] ?></button>
                      </form>
                  </div>
--- system/system_project.php
+++ system/system_project.php
@@ -7,43 +7,48 @@ session_start();
 //variablen devinieren
 $project = array();
 
-//Alle projekte holen
-$query = pick_all_project();
+// Prijekt Archivieren
+if (isset($_POST['archive'])){
+    $db = new class_database();
+    $project_name = $_SESSION['name'];
+    $query_archive = $db->mysql->query("SELECT * FROM project WHERE projectname = '$project_name'")->fetch_assoc();
+    $archive = $query_archive['archive'];
+
+    if ($archive == 'false') {
+        $query_info = $db->mysql->query("UPDATE project SET archive = 'true' WHERE projectname = '$project_name'");
+    }
+    elseif ($archive == 'true') {
+        $query_info = $db->mysql->query("UPDATE project SET archive = 'false' WHERE projectname = '$project_name'");
+    }
+    $db->close_connection();
+}
+
+
 
 //Projekt hinzufügen
 if (isset($_POST['add'])){
     add_project($_POST['projectname'],$_POST['description'],$_POST['budget']);
 }
 
+//Alle projekte holen
+$query = pick_all_project();
 
-if (isset($_POST['project'])) {
 
+if (isset($_GET['Test'])) {
     $db = new class_database();
-    $projectname = $_POST['project'];
+    $projectname = $_GET['Test'];
     $query_info = $db->mysql->query("SELECT * FROM project LEFT JOIN time ON time.projectid = project.id WHERE projectname = '$projectname'")->fetch_assoc();
-    $project['projectname'] = $query_info['projectname'];
+    $_SESSION['name'] = $project['projectname'] = $query_info['projectname'];
     $project['description'] = $query_info['description'];
     $project['budget'] = $query_info['budget'];
     $project['archive'] = $query_info['archive'];
     $project['nr'] = $query_info['projectnr'];
+    $ist_time = chart_project($project['projectname']);
     $db->close_connection();
-}
-
-$ist_time = chart_project($project['projectname']);
-
-if (isset($_POST['archive'])){
-    $db = new class_database();
-    $project_name = $_SESSION['projectname'];
-    $query_archive = $db->mysql->query("SELECT * FROM project WHERE projectname = '$project_name'")->fetch_assoc();
-    $archive = $query_archive['archive'];
-
-    if ($archive == 'false') {
-        $query_info = $db->mysql->query("UPDATE project SET archive = 'true' WHERE projectname = '$project_name'");
-
-    }
-    elseif ($archive == 'true') {
-        $query_info = $db->mysql->query("UPDATE project SET archive = 'false' WHERE projectname = '$project_name'");
-
-    }
-    $db->close_connection();
+        ?>
+        <script type="text/javascript">
+            var budget = "<? echo $project['budget']; ?>";
+            var ist_time = "<? echo $ist_time; ?>";
+        </script>
+<?php
 }
