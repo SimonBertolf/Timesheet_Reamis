30d90f35308779281e7731fdd3797ac5073af125
Simon Bertolf <simon.bertolf@reamis.ch>
2019-12-18 09:23:08 +0100

    mainpage angefangen

--- SQL/SQL_export/structure.sql
+++ SQL/SQL_export/structure.sql
@@ -1,9 +1,6 @@
-
--- Exportiere Datenbank Struktur für timesheet_reamis
 CREATE DATABASE IF NOT EXISTS `timesheet_reamis` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `timesheet_reamis`;
 
--- Exportiere Struktur von Tabelle timesheet_reamis.authorization
 CREATE TABLE IF NOT EXISTS `authorization` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `projectid` int(11) NOT NULL,
@@ -15,8 +12,6 @@ CREATE TABLE IF NOT EXISTS `authorization` (
   CONSTRAINT `FK_authorization_user` FOREIGN KEY (`userid`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8 COMMENT='Berechtigung wer an welchem Projeckt arbeitet';
 
-
--- Exportiere Struktur von Tabelle timesheet_reamis.project
 CREATE TABLE IF NOT EXISTS `project` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `projectname` varchar(50) NOT NULL DEFAULT '',
@@ -28,15 +23,11 @@ CREATE TABLE IF NOT EXISTS `project` (
   UNIQUE KEY `projectname` (`projectname`)
 ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COMMENT='Zum erfassen von Projekten';
 
--- Exportiere Daten aus Tabelle timesheet_reamis.project: ~-1 rows (ungefähr)
 DELETE FROM `project`;
-/*!40000 ALTER TABLE `project` DISABLE KEYS */;
 INSERT INTO `project` (`id`, `projectname`, `description`, `budget`, `archive`, `projectnr`) VALUES
 	(1, 'Ferien', 'Abwesenheit durch Ferien', 0, 'false', '1'),
 	(2, 'Krankheit', 'Abwesenheit durch Krankheit', 0, 'false', '2'),
-/*!40000 ALTER TABLE `project` ENABLE KEYS */;
 
--- Exportiere Struktur von Tabelle timesheet_reamis.time
 CREATE TABLE IF NOT EXISTS `time` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `projectid` int(11) NOT NULL,
@@ -53,7 +44,6 @@ CREATE TABLE IF NOT EXISTS `time` (
   CONSTRAINT `FK_time_user` FOREIGN KEY (`userid`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8 COMMENT='Für die Arbeitszeiten';
 
--- Exportiere Struktur von Tabelle timesheet_reamis.user
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL DEFAULT '',
---
+++ SQL/SQL_ubdate/user_ubdate_05_12.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user` (`id`, `username`, `password`, `name`, `quote`, `typ`, `status`) VALUES
+	(1, 'A', '8d5e957f297893487bd98fa830fa6413', 'Admin', 8, 'admin', 'active'),
+	(2, 'C', '8d5e957f297893487bd98fa830fa6413', 'Controller', 8, 'controller', 'active'),
+	(3, 'SB', '8d5e957f297893487bd98fa830fa6413', 'Simon Bertolf', 8, 'standard', 'active');
+
