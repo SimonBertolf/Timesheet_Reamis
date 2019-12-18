71437ce387701c83cc3eee1307c3faf167ccefc3
Simon Bertolf <simon.bertolf@reamis.ch>
2019-12-18 09:20:27 +0100

    mainpage angefangen

--- SQL/SQL_export/structure.sql
+++ SQL/SQL_export/structure.sql
@@ -1,16 +1,3 @@
--- --------------------------------------------------------
--- Host:                         localhost
--- Server Version:               5.5.62-0+deb8u1 - (Debian)
--- Server Betriebssystem:        debian-linux-gnu
--- HeidiSQL Version:             10.2.0.5717
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 
 -- Exportiere Datenbank Struktur für timesheet_reamis
 CREATE DATABASE IF NOT EXISTS `timesheet_reamis` /*!40100 DEFAULT CHARACTER SET utf8 */;
@@ -26,12 +13,8 @@ CREATE TABLE IF NOT EXISTS `authorization` (
   KEY `projectid` (`projectid`),
   CONSTRAINT `FK_authorization_project` FOREIGN KEY (`projectid`) REFERENCES `project` (`id`),
   CONSTRAINT `FK_authorization_user` FOREIGN KEY (`userid`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Berechtigung wer an welchem Projeckt arbeitet';
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8 COMMENT='Berechtigung wer an welchem Projeckt arbeitet';
 
--- Exportiere Daten aus Tabelle timesheet_reamis.authorization: ~-1 rows (ungefähr)
-DELETE FROM `authorization`;
-/*!40000 ALTER TABLE `authorization` DISABLE KEYS */;
-/*!40000 ALTER TABLE `authorization` ENABLE KEYS */;
 
 -- Exportiere Struktur von Tabelle timesheet_reamis.project
 CREATE TABLE IF NOT EXISTS `project` (
@@ -40,14 +23,17 @@ CREATE TABLE IF NOT EXISTS `project` (
   `description` varchar(50) DEFAULT NULL COMMENT 'project description',
   `budget` int(11) DEFAULT NULL COMMENT 'time budget',
   `archive` enum('true','false') NOT NULL DEFAULT 'false' COMMENT 'true for finished projects',
+  `projectnr` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `projectname` (`projectname`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Zum erfassen von Projekten';
+  UNIQUE KEY `projectname` (`projectname`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COMMENT='Zum erfassen von Projekten';
 
 -- Exportiere Daten aus Tabelle timesheet_reamis.project: ~-1 rows (ungefähr)
 DELETE FROM `project`;
 /*!40000 ALTER TABLE `project` DISABLE KEYS */;
+INSERT INTO `project` (`id`, `projectname`, `description`, `budget`, `archive`, `projectnr`) VALUES
+	(1, 'Ferien', 'Abwesenheit durch Ferien', 0, 'false', '1'),
+	(2, 'Krankheit', 'Abwesenheit durch Krankheit', 0, 'false', '2'),
 /*!40000 ALTER TABLE `project` ENABLE KEYS */;
 
 -- Exportiere Struktur von Tabelle timesheet_reamis.time
@@ -56,25 +42,22 @@ CREATE TABLE IF NOT EXISTS `time` (
   `projectid` int(11) NOT NULL,
   `userid` int(11) NOT NULL,
   `date` date NOT NULL,
-  `time` int(11) NOT NULL COMMENT 'Worktime in houres',
-  `description` varchar(50) DEFAULT NULL COMMENT 'Worktask description ',
+  `time` float NOT NULL COMMENT 'Worktime in houres',
+  `task` varchar(50) DEFAULT NULL COMMENT 'Worktask description ',
+  `start` time DEFAULT NULL COMMENT 'Start time',
+  `stop` time DEFAULT NULL COMMENT 'End time',
   PRIMARY KEY (`id`),
   KEY `projectid` (`projectid`),
   KEY `userid` (`userid`),
   CONSTRAINT `FK_time_project` FOREIGN KEY (`projectid`) REFERENCES `project` (`id`),
   CONSTRAINT `FK_time_user` FOREIGN KEY (`userid`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Für die Arbeitszeiten';
-
--- Exportiere Daten aus Tabelle timesheet_reamis.time: ~-1 rows (ungefähr)
-DELETE FROM `time`;
-/*!40000 ALTER TABLE `time` DISABLE KEYS */;
-/*!40000 ALTER TABLE `time` ENABLE KEYS */;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8 COMMENT='Für die Arbeitszeiten';
 
 -- Exportiere Struktur von Tabelle timesheet_reamis.user
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL DEFAULT '',
-  `pasword` varchar(10) NOT NULL COMMENT 'md5 ',
+  `password` varchar(50) NOT NULL COMMENT 'md5 ',
   `name` varchar(50) NOT NULL DEFAULT '' COMMENT 'full name',
   `quote` int(11) NOT NULL DEFAULT '8' COMMENT 'daily quote in houres',
   `typ` enum('admin','controller','standard') NOT NULL DEFAULT 'standard',
@@ -82,13 +65,4 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   KEY `userid` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Zum erfassen der Benutzer';
-
--- Exportiere Daten aus Tabelle timesheet_reamis.user: ~-1 rows (ungefähr)
-DELETE FROM `user`;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='Zum erfassen der Benutzer';
