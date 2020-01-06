bebda7e05105350f8fdc4d65ead1a9da3bfecd6a
Simon Bertolf <simon.bertolf@reamis.ch>
2020-01-06 08:46:41 +0100

    SQL

--- SQL/SQL_ubdate/user_ubdate_03_01.sql
+++ SQL/SQL_ubdate/user_ubdate_03_01.sql
@@ -4,7 +4,8 @@ DELETE FROM `project`;
 INSERT INTO `project` (`id`, `projectname`, `description`, `budget`, `archive`, `projectnr`) VALUES
 	(1, 'Ferien', 'Abwesenheit durch Ferien', 0, 'false', '1'),
 	(2, 'Krankheit', 'Abwesenheit durch Krankheit', 0, 'false', '2'),
-	(3, 'Feiertag', 'Abwesenheit durch Feiertage', 0, 'false', '3');
+	(3, 'Feiertag', 'Abwesenheit durch Feiertage', 0, 'false', '3'),
+	(4, 'Timesheet Reamis', 'Ein Timesheet Projekt fpr Reamis AG', 500, 'false', 'A1000');
 /*!40000 ALTER TABLE `project` ENABLE KEYS */;
 
 -- Exportiere Daten aus Tabelle timesheet_reamis.user: ~-1 rows (ungefähr)
@@ -13,5 +14,6 @@ DELETE FROM `user`;
 INSERT INTO `user` (`id`, `username`, `password`, `name`, `quote`, `typ`, `status`) VALUES
 	(1, 'Admin', '8d5e957f297893487bd98fa830fa6413', 'Admin', 8, 'admin', 'active'),
 	(2, 'Controller', '8d5e957f297893487bd98fa830fa6413', 'Controller', 8, 'controller', 'active'),
-	(3, 'Test', '8d5e957f297893487bd98fa830fa6413', 'Test User', 8.5, 'standard', 'active');
+	(3, 'Test', '8d5e957f297893487bd98fa830fa6413', 'Test User', 8.5, 'standard', 'active'),
+	(4, 'Simon', '8d5e957f297893487bd98fa830fa6413', 'Simon Bertolf', 8.5, 'standard', 'active');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
